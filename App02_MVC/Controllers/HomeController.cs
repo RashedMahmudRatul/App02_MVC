0b2682f5b4a0a18d25615ebd863d12abf4677034
Rashed Mahmud <rashedmahmud68@gmail.com>
2020-10-12 07:51:52 +0600

    Add project files.

---
+++ App02_MVC/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using App02_MVC.Models;
+
+namespace App02_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            //return View("Test");
+            return View();
+        }
+
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Test()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View("ourpage");
+        }
+
+        public IActionResult ShowFiles()
+        {
+            return View("FileList");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
